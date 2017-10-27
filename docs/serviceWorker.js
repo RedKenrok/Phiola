a78b49fcf015f2cd45f9dfd599922b0138a942ca
Ron Dekker <RedKenrok@gmail.com>
2017-10-27 16:53:12 +0200

    Update service worker

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -4,11 +4,9 @@ if ('serviceWorker' in navigator) {
 			.register('serviceWorker.js')
 			.then(
 				function(registration) {
-					// Registration successful.
 					console.log('ServiceWorker registration successful with scope: ', registration.scope);
 				},
 				function(error) {
-					// Registration failed.
 					console.log('ServiceWorker registration failed: ', error);
 				});
 		});
